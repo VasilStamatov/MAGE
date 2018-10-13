14a0fb78184f005188bfe5bcac56f7dd8d4a4e9b
VasilStamatov <15113857+VasilStamatov@users.noreply.github.com>
2018-10-13 17:35:37 +0100

    Added the component mask of the entities to the entity manage.

--- mage/ecs/include/ecs/EntityManager.h
+++ mage/ecs/include/ecs/EntityManager.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "ComponentMask.h"
 #include "Entity.h"
 
-#include <queue>
+#include <vector>
 
 namespace mage
 {
@@ -12,14 +13,20 @@ namespace ecs
 class EntityManager
 {
 private:
-  std::queue<std::uint32_t> m_freeEntityIds;
-  std::uint32_t m_lastFreeEntityId;
+  std::vector<std::uint32_t> m_freeEntityIds;
+  std::vector<ComponentMask> m_entityComponentMasks;
 
 public:
   EntityManager();
 
   Entity CreateEntity();
   void RecycleEntity(Entity _entity);
+
+  ComponentMask GetComponentMaskForEntity(Entity _entity) const noexcept;
+
+  void SetComponentBit(Entity _entity, std::uint32_t _componentBit) noexcept;
+  void UnsetComponentBit(Entity _entity, std::uint32_t _componentBit) noexcept;
+  void ResetComponentMask(Entity _entity) noexcept;
 };
 
 } // namespace ecs
--- mage/ecs/src/EntityManager.cpp
+++ mage/ecs/src/EntityManager.cpp
@@ -6,7 +6,7 @@ namespace ecs
 {
 
 EntityManager::EntityManager()
-    : m_freeEntityIds(), m_lastFreeEntityId(0)
+    : m_freeEntityIds(), m_entityComponentMasks(0)
 {
 }
 
@@ -16,12 +16,13 @@ Entity EntityManager::CreateEntity()
 
     if (m_freeEntityIds.size() > 0)
     {
-        entityId = m_freeEntityIds.front();
-        m_freeEntityIds.pop();
+        entityId = m_freeEntityIds.back();
+        m_freeEntityIds.pop_back();
     }
     else
     {
-        entityId = m_lastFreeEntityId++;
+        entityId = m_entityComponentMasks.size();
+        m_entityComponentMasks.emplace_back();
     }
 
     return Entity(entityId);
@@ -29,7 +30,27 @@ Entity EntityManager::CreateEntity()
 
 void EntityManager::RecycleEntity(Entity _entity)
 {
-    m_freeEntityIds.push(_entity.m_iD);
+    m_freeEntityIds.emplace_back(_entity.m_id);
+}
+
+ComponentMask EntityManager::GetComponentMaskForEntity(Entity _entity) const noexcept
+{
+    return m_entityComponentMasks[_entity.m_id];
+}
+
+void EntityManager::SetComponentBit(Entity _entity, std::uint32_t _componentBit) noexcept
+{
+    m_entityComponentMasks[_entity.m_id].SetComponentBit(_componentBit);
+}
+
+void EntityManager::UnsetComponentBit(Entity _entity, std::uint32_t _componentBit) noexcept
+{
+    m_entityComponentMasks[_entity.m_id].UnsetComponentBit(_componentBit);
+}
+
+void EntityManager::ResetComponentMask(Entity _entity) noexcept
+{
+    m_entityComponentMasks[_entity.m_id].Reset();
 }
 
 } // namespace ecs
