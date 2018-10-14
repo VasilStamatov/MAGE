a120e6314dd34220b934ee826f17e61f1e6a6f18
VasilStamatov <15113857+VasilStamatov@users.noreply.github.com>
2018-10-14 19:50:31 +0100

    Added unit tests for the component struct

--- mage/ecs/include/ecs/Component.h
+++ mage/ecs/include/ecs/Component.h
@@ -24,7 +24,7 @@ std::uint32_t GetComponentTypeId() noexcept
     static_assert(std::is_base_of<Component, ComponentType>::value, "Must inherit from Component");
     static_assert(std::is_standard_layout<ComponentType>::value, "Must have standard layout (all members public, no virtual functions)");
 
-    static std::uint32_t s_componentTypeId = GenerateUniqueComponentId();
+    static std::uint32_t s_componentTypeId = internal::GenerateUniqueComponentId();
     return s_componentTypeId;
 }
 
